cb8bcf6288f5fd04efeea6ff537541785116c408
Programmer3314 <53499567+Programmer3314@users.noreply.github.com>
2020-05-12 22:37:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,8 @@ apt-get --assume-yes install samba
 (echo $REMOTEPASSWORD; echo $REMOTEPASSWORD) | smbpasswd -a -s $PRIMARYUSER
 cp /etc/samba/smb.confBACKUP /etc/samba/smb.conf 
 cp /etc/samba/smb.conf /etc/samba/smb.confBACKUP
-sed -i "s/<teamnumber>/$TEAMNUMBER/g" smb.conf_map_team_dir
-sed -i "s/<primaryuser>/$PRIMARYUSER/g" smb.conf_map_team_dir
-cat smb.conf_map_team_dir >> /etc/samba/smb.conf
+cp smb.conf_map_team_dir smb.conf_map_team_dir_updated
+sed -i "s/<teamnumber>/$TEAMNUMBER/g" smb.conf_map_team_dir_updated
+sed -i "s/<primaryuser>/$PRIMARYUSER/g" smb.conf_map_team_dir_updated
+cat smb.conf_map_team_dir_updated >> /etc/samba/smb.conf
 sudo service smbd restart
