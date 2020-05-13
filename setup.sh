c90660dc473c419717705a32fa763aa2d93536f1
Programmer3314 <53499567+Programmer3314@users.noreply.github.com>
2020-05-13 19:46:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,3 +31,11 @@ sudo apt-get --assume-yes install python3-pip
 sudo pip3 install -U pip testresources setuptools
 sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
+
+echo
+echo Setup TensorFlow Object Detection API
+echo
+cd ..
+git clone https://github.com/tensorflow/models.git
+
+mv ./JetsonNanoSetup/objectdetectiontools ./
