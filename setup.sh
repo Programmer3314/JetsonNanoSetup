33753796cbeede6b6530e5f2cff92c9add8cf243
Programmer3314 <53499567+Programmer3314@users.noreply.github.com>
2020-05-12 21:08:48 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo Hello World 
+
+PRIMARYUSER=rich
+TEAMNUMBER=3314
+REMOTEPASSWORD=xxxxx
+
+echo Jetson Nano Setup
+
+sudo apt-get update
+sudo apt-get install samba
+Y
+
+sudo smbpasswd -a $PRIMARYUSER
+$REMOTEPASSWORD
+$REMOTEPASSWORD
+
+sudo cp /etc/samba/smb.conf /etc/samba/smb.confBACKUP
+
+#copy config file addition
+sudo service smbd restart
