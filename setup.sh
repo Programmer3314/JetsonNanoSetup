e3aab4365efab70d04e65f49c78239f0d003847a
Programmer3314 <53499567+Programmer3314@users.noreply.github.com>
2020-05-13 11:37:56 -0400

    add TensorFlow

--- setup.sh
+++ setup.sh
@@ -20,4 +20,14 @@ cp smb.conf_map_team_dir smb.conf_map_team_dir_updated
 sed -i "s/<teamnumber>/$TEAMNUMBER/g" smb.conf_map_team_dir_updated
 sed -i "s/<primaryuser>/$PRIMARYUSER/g" smb.conf_map_team_dir_updated
 cat smb.conf_map_team_dir_updated >> /etc/samba/smb.conf
-sudo service smbd restart
+service smbd restart
+
+
+echo
+echo Setup TensorFlow
+echo 
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+sudo apt-get install python3-pip
+sudo pip3 install -U pip testresources setuptools
+sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
