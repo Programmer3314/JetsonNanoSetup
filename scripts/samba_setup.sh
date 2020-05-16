a82cbd0f129eacb655c9c8f64a133481c9c77006
rakar <rich.kopelow@gmail.com>
2020-05-16 13:20:40 -0400

    Update samba_setup.sh

--- scripts/samba_setup.sh
+++ scripts/samba_setup.sh
@@ -4,6 +4,9 @@ PRIMARYUSER=rich
 TEAMNUMBER=3314
 REMOTEPASSWORD=xxxxx
 
+SETUPHOME=/$TEAMNUMBER/JestonNanoSetup/
+SCRIPTHOME=$SETUPHOME/scripts
+
 echo
 echo Setup Samba Network Access to Team Directory
 echo
@@ -12,8 +15,8 @@ apt-get --assume-yes install samba
 (echo $REMOTEPASSWORD; echo $REMOTEPASSWORD) | smbpasswd -a -s $PRIMARYUSER
 cp /etc/samba/smb.confBACKUP /etc/samba/smb.conf 
 cp /etc/samba/smb.conf /etc/samba/smb.confBACKUP
-cp /$TEAMNUMBER/scripts/data/smb.conf_map_team_dir /$TEAMNUMBER/scripts/data/smb.conf_map_team_dir_updated
-sed -i "s/<teamnumber>/$TEAMNUMBER/g" /$TEAMNUMBER/scripts/data/smb.conf_map_team_dir_updated
-sed -i "s/<primaryuser>/$PRIMARYUSER/g" /$TEAMNUMBER/scripts/data/smb.conf_map_team_dir_updated
-cat /$TEAMNUMBER/scripts/data/smb.conf_map_team_dir_updated >> /etc/samba/smb.conf
+cp /$SCRIPTHOME/data/smb.conf_map_team_dir /$SCRIPTHOME/data/smb.conf_map_team_dir_updated
+sed -i "s/<teamnumber>/$TEAMNUMBER/g" /$SCRIPTHOME/data/smb.conf_map_team_dir_updated
+sed -i "s/<primaryuser>/$PRIMARYUSER/g" /$SCRIPTHOME/data/smb.conf_map_team_dir_updated
+cat /$SCRIPTHOME/data/smb.conf_map_team_dir_updated >> /etc/samba/smb.conf
 service smbd restart
