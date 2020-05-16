731994dbb94b9e0b67990b0256c69ae6824f9915
rakar <rich.kopelow@gmail.com>
2020-05-16 13:45:09 -0400

    Update and rename setup.sh to scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-PRIMARYUSER=rich
-TEAMNUMBER=3314
-REMOTEPASSWORD=xxxxx
+source parameters.sh
 
 echo
 echo Jetson Nano Setup
@@ -11,16 +9,7 @@ echo
 echo
 echo Setup Samba Network Access to Team Directory
 echo
-apt-get update
-apt-get --assume-yes install samba
-(echo $REMOTEPASSWORD; echo $REMOTEPASSWORD) | smbpasswd -a -s $PRIMARYUSER
-cp /etc/samba/smb.confBACKUP /etc/samba/smb.conf 
-cp /etc/samba/smb.conf /etc/samba/smb.confBACKUP
-cp smb.conf_map_team_dir smb.conf_map_team_dir_updated
-sed -i "s/<teamnumber>/$TEAMNUMBER/g" smb.conf_map_team_dir_updated
-sed -i "s/<primaryuser>/$PRIMARYUSER/g" smb.conf_map_team_dir_updated
-cat smb.conf_map_team_dir_updated >> /etc/samba/smb.conf
-service smbd restart
+sudo samba_setup.sh
 
 
 echo
